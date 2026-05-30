ce2ba786d45a91f7a1cce0aa5b7ac8fde6078b42
punith <punitiptur2@gmail.com>
2026-05-30 08:46:18 +0530

    400

--- app.py
+++ app.py
@@ -10,5 +10,6 @@ def hello():
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 2000))
+
+    port = int(os.environ.get("PORT", 4000))
     app.run(debug=True,host='0.0.0.0',port=port)
